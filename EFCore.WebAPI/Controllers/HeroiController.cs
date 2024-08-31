4c0542e22a0808f5ef518e626e606b0dded4c293
Emanuel da Silva <emanuel.silva@pixeon.com>
2024-08-31 12:16:49 -0300

    projeto curso entityframework core

---
+++ EFCore.WebAPI/Controllers/HeroiController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore.Dominio;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroiController : ControllerBase
+    {
+        private readonly IEFCoreRepository _repo;
+
+        public HeroiController(IEFCoreRepository repo)
+        {
+            _repo = repo;
+        }
+        // GET: api/Heroi
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var herois = await _repo.GetAllHerois(true);
+
+                return Ok(herois);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // GET: api/Heroi/5
+        [HttpGet("{id}", Name = "GetHeroi")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var herois = await _repo.GetHeroiById(id, true);
+
+                return Ok(herois);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // POST: api/Heroi
+        [HttpPost]
+        public async Task<IActionResult> Post(Heroi model)
+        {
+            try
+            {
+                _repo.Add(model);
+
+                if (await _repo.SaveChangeAsync())
+                {
+                    return Ok("BAZINGA");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+
+            return BadRequest("Não Salvou");
+        }
+
+        // PUT: api/Heroi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Heroi model)
+        {
+            try
+            {
+                var heroi = await _repo.GetHeroiById(id);
+                if (heroi != null)
+                {
+                    _repo.Update(model);
+
+                    if (await _repo.SaveChangeAsync())
+                        return Ok("BAZINGA");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+
+            return BadRequest($"Não Deletado!");
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var heroi = await _repo.GetHeroiById(id);
+                if (heroi != null)
+                {
+                    _repo.Delete(heroi);
+
+                    if (await _repo.SaveChangeAsync())
+                        return Ok("BAZINGA");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+
+            return BadRequest($"Não Deletado!");
+        }
+    }
+}
